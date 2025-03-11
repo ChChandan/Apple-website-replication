22301432818dd8b68c06294d791a7f99dad01aec
ChChandan <chandanvja@gmail.com>
2025-03-11 18:21:30 +1100

    final commit

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,9 +1,10 @@
 import React, { useRef } from "react";
 import { chipImg, frameImg, frameVideo } from "../utils";
 import { useGSAP } from "@gsap/react";
+import { animateWithGsap } from "../utils/animation";
 import gsap from "gsap";
 const HowItWorks = () => {
-    const videoRef=useRef();
+  const videoRef = useRef();
   useGSAP(() => {
     gsap.from("#chip", {
       scrollTrigger: { trigger: "#chip", start: "20% bottom" },
@@ -12,7 +13,14 @@ const HowItWorks = () => {
       duration: 2,
       ease: "power2.inOut",
     });
-  });
+
+    animateWithGsap(".g_fadeIn", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  }, []);
   return (
     <section className="common-padding">
       <div className="screen-max-width">
@@ -35,15 +43,49 @@ const HowItWorks = () => {
                 alt="frame"
                 className="bg-transparent relative z-10"
               />
-             
             </div>
             <div className="hiw-video">
-                <video className="pointer-events-none" playsInline preload="none" muted autoPlay ref={videoRef}>
-                  <source src={frameVideo} type="video/mp4"></source>
-                </video>
-              </div>
+              <video
+                className="pointer-events-none"
+                playsInline
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={frameVideo} type="video/mp4"></source>
+              </video>
+            </div>
           </div>
-          <p className="text-gray text-center font-semibold mt-3">Honkai: Star Rail</p>
+          <p className="text-gray text-center font-semibold mt-3">
+            Honkai: Star Rail
+          </p>
+        </div>
+        <div className="hiw-text-container">
+          <div className="flex flex-1 justify-center flex-col">
+            <p className="hiw-text g_fadeIn">
+              A17 Pro is an entirely new class of iPhone chip that delivers our{" "}
+              {""}
+              <span className="text-white">
+                best graphic performance by far
+              </span>
+            </p>
+         
+
+          <p className=" hiw-text g_fadeIn">
+            Mobile {""}
+            <span className="text-white">
+              {" "}
+              games will look and feel so immersive
+            </span>
+            with incredibly detailed environments and characters
+          </p>
+</div>
+          <div className="flex-1 flex justify-center flex-col g_fadeIn">
+            <p className="hiw-text">New</p>
+            <p className="hiw-bigtext">Pro-class GPU</p>
+            <p className="hiw-text">with 6 cores</p>
+          </div>
         </div>
       </div>
     </section>
